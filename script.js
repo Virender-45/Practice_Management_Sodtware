509a446e27646a75099c5af2fdae1b33def2e341
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-10-04 07:32:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// const observer = new IntersectionObserver((entries) =>  {
+//     entries.forEach((entries) => {
+//         console.log(entry)
+//         if (entries.isIntersecting) {
+//             entry.target.classList.add('show');
+//         }
+//         else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hero-section-image');
+// hiddenElements.forEach((el) => observer.observe(el));
